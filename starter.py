b423397719b3c138069cd1585f236ac43dfa2fc0
Burhan Cikili <35443804+bcikili@users.noreply.github.com>
2024-02-06 18:14:34 -0600

    added starter code

---
+++ starter.py
@@ -0,0 +1,28 @@
+# Dependencies
+import requests
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+# list of TV show titles to query
+tv_shows = ["Altered Carbon", "Grey's Anatomy", "This is Us", "The Flash",
+            "Vikings", "Shameless", "Arrow", "Peaky Blinders", "Dirk Gently"]
+
+# TV Maze show search base URL
+base_url = "http://api.tvmaze.com/search/shows?q="
+
+# set up lists to hold response data for name and rating
+titles = []
+ratings = []
+
+# loop through TV show titles, make requests and parse
+for show in tv_shows:
+    target_url = base_url + show
+    response = requests.get(target_url).json()
+    titles.append(response[0]['show']['name'])
+    ratings.append(response[0]['show']['rating']['average'])
+
+# create DataFrame
+shows_df = pd.DataFrame({
+    "title": titles,
+    "rating": ratings
+})
